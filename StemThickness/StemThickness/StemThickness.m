0aaac37b1411e0f62fd585f0c25573fb26f5222b
schriftgestalt <georg.seifert@mac.com>
2021-03-23 21:32:09 +0100

    use text size according to user UI size setting

--- StemThickness/StemThickness/StemThickness.m
+++ StemThickness/StemThickness/StemThickness.m
@@ -225,7 +225,7 @@ static NSColor *pointColor = nil;
 	NSPoint thisDistanceCenter = GSMiddlePointStem(onCurve, cross);
 	[color set];
 	[self drawDashedStrokeA:onCurve b:cross];
-	CGFloat fontSize = 10 * pow(_scale, 0.1);
+	CGFloat fontSize = handleSize * 1.5 * pow(_scale, 0.1);
 	[distanceShowed drawBadgeAtPoint:thisDistanceCenter size:fontSize color:NSColor.textBackgroundColor backgroundColor:color alignment:GSCenterCenter visibleInRect:NSMakeRect(NSNotFound, 0, 0, 0)];
 	[self drawPoint:cross size:zoomedHandleSize color:color];
 }
